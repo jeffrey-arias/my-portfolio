01856ccb1e1bbd54528a4fdeefb5bc6a535e53b7
Jeffrey Arias <jeffrey.e.arias@gmail.com>
2021-03-27 04:27:53 +0800

    enhancments to landing page

--- src/components/body.component.js
+++ src/components/body.component.js
@@ -31,8 +31,8 @@ export default class MainBody extends Component {
             <div className="mainBody">
                 <main role="main" className="container" onMouseOver={hideDivs} onMouseOut={showDivs}>
                         <div className="mainText">Hello</div>
-                        <div className="tinyText">&lt; <text id="mobile">tap</text><text id="desktop">mouseover</text> for fun &gt;</div>
-                        <div className="text">I am Jeff.</div>
+                        <div className="tinyText"><mark>&lt; <text id="mobile">tap</text><text id="desktop">mouseover</text> for fun /&gt;</mark></div>
+                        <div className="text">I am <b>Jeff</b>.</div>
                         <div id="visibleText" className="text">A Full-Stack Developer.</div> 
                         <div id="hiddenText" className="strikethroughtextHidden">(A Full-Stack     Developer.)</div> 
                         <div id="hiddenTextFunny" className="textHidden">I turn caffeine into lines of code.</div> 
@@ -41,10 +41,10 @@ export default class MainBody extends Component {
                 </main>
                 <nav class="navbar fixed-bottom navbar-expand-sm navbar-dark">
                     <div class="mx-auto">
-                    <div className="poweredByText">This site is made with: </div>
-                        <img height={30} title={'React.js'} width={30} src={reactLogo.default}/>
-                        <img height={30} title={'Bootstrap'} width={30} src={bootstrapLogo.default}/>
-                        <img height={40} title={'jQuery'} width={40} src={jQueryLogo.default}/>
+                    <div className="poweredByText">I made this site is made with: </div>
+                        <img className="imgIcons" height={30} title={'React.js'} width={30} src={reactLogo.default}/>
+                        <img className="imgIcons" height={30} title={'Bootstrap'} width={30} src={bootstrapLogo.default}/>
+                        <img className="imgIcons" height={40} title={'jQuery'} width={40} src={jQueryLogo.default}/>
                     </div> 
                 </nav>
             </div>  
